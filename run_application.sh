adaa9ef357e7a2baa20cc13a6e05bc81039988ad
akerkar2005 <akerkar@purdue.edu>
2025-07-24 14:50:24 -0400

    updated README and moved run_application to root

--- run_application.sh
+++ run_application.sh
@@ -6,4 +6,4 @@ set -e
 cd "$(dirname "$(realpath $0)")"
 
 sudo npm install -g concurrently
-concurrently "cd ../server; npm run dev" "npm run dev"
+concurrently "cd server; npm run dev" "cd client; npm run dev"
